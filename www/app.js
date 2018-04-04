4bcf27c981545c439a8b1636d589d406ea1e1175
andrew <andrewxhill@gmail.com>
2018-04-04 12:43:02 -0700

    new-config

---
+++ www/app.js
@@ -0,0 +1,21 @@
+// For any third party dependencies, like jQuery, place them in the lib folder.
+
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        app: '../app',
+      'jquery': ['jquery-3.3.1.min'],
+      'fancybox': ['jquery.fancybox.min'],
+      'justified': ['jquery.justifiedGallery.min'],
+      'humandate': ['humandate.min'],
+      'promiseNode': ['promise.node'],
+      'ipfs': ['ipfs.min']
+    }
+});
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['app/main']);
